3937a601d9b37a1293de2963b4fd61ec332368e6
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2018-03-19 07:25:45 -0400

    Added copy all *.conf from all apps into the nginx-confs dir.

--- spin-up-env.sh
+++ spin-up-env.sh
@@ -2,16 +2,8 @@
 #
 pwd
 
-echo "Allow internal composer package manager"
-mkdir -p ~/.composer && cp ./resources/composer-config.json ~/.composer/config.json
-
-mkdir -p ~/code ~/code/nginx-configs
+# Copy all applications nginx files into a single directory.
+cp ~/code/*/web-env/*.conf ~/code/nginx-confs/
 
 #docker-compose --project-name=marketing_web_env up -d --no-recreate --remove-orphans
 docker-compose --project-name=marketing_web_env up --no-recreate --remove-orphans
-
-# Copy the composer configuration to the app container
-docker cp ./resources/add-internal-composer.sh centos-apps:/add-internal-composer.sh
-docker exec centos-apps "chown" "root:root" "/add-internal-composer.sh"
-docker exec centos-apps "chmod" "a+x" "/add-internal-composer.sh"
-docker exec centos-apps "/add-internal-composer.sh"
