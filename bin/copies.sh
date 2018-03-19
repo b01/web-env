e24ae5b9d5c88c36979e9a78210787dfc22d56b1
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2018-03-19 17:06:07 -0400

    Moved copy functions to a collective script.

--- bin/copies.sh
+++ bin/copies.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+pwd
+
+# Copy SSH keys over to the containers.
 if [[ -f "~/.ssh/" ]]; then
     docker cp ~/.ssh/. centos-apps54:/root/.ssh
     docker cp ~/.ssh/. centos-apps:/root/.ssh
@@ -7,3 +10,7 @@ fi
 
 docker exec centos-apps54 "chown" "-R" "root:root" "/root/.ssh/"
 docker exec centos-apps "chown" "-R" "root:root" "/root/.ssh/"
+
+# Copy all applications nginx files into the mapped NginX vhost configuration directory.
+cp ~/code/*/web-env/*.conf ~/code/nginx-confs/
+docker exec centos-apps "nginx" "-s" "reload"
--- bin/spin-up-env.sh
+++ bin/spin-up-env.sh
@@ -2,8 +2,5 @@
 #
 pwd
 
-# Copy all applications nginx files into a single directory.
-cp ~/code/*/web-env/*.conf ~/code/nginx-confs/
-
 #docker-compose --project-name=marketing_web_env up -d --no-recreate --remove-orphans
 docker-compose --project-name=marketing_web_env up --no-recreate --remove-orphans
