e64de7c00caec95a6da8e2f9e69a545d8667aff1
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2018-03-19 16:03:01 -0400

    * Cleaned up docker composer configuration. * Changed to copy ssh keys to apps container.

--- bin/copy-keys.sh
+++ bin/copy-keys.sh
@@ -2,6 +2,8 @@
 
 if [[ -f "~/.ssh/" ]]; then
     docker cp ~/.ssh/. centos-apps54:/root/.ssh
+    docker cp ~/.ssh/. centos-apps:/root/.ssh
 fi
 
 docker exec centos-apps54 "chown" "-R" "root:root" "/root/.ssh/"
+docker exec centos-apps "chown" "-R" "root:root" "/root/.ssh/"
