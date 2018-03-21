f5ed29b623ccec8dd6aa6e11261de64098d48dc3
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2018-03-21 17:50:23 -0400

    Added untested changes to the config-app script.
    * Added step to restart the Nginx server in the Nginx container.
    * Added step to generate an SSL certificate for app.
    * Read app name from second parameter.
    * Conditionally clone repo when its directory is not present.
    * Added step run check and run a build script if the app has one.

--- bin/config-app.sh
+++ bin/config-app.sh
@@ -6,9 +6,20 @@ APP_NAME=$(echo "${1}" | perl -pe 's#.+?/(.+?).git$#\1#g')
 #APP_NAME=$(echo "${1}" | sed -E 's#[^/]+/(.+).git#\1#g')
 printf "Spinning up ${APP_NAME}\n"
 
-# 2. Clone the repo.
+# Allow an app container to be specified.
+APP_CONTAINER="centos-apps"
+if [ ! -z "${2}" ]; then
+    APP_CONTAINER="${2}"
+fi
+
+# 1.a Change to the docker apps dir.
 cd "${APPS_DIR}"
-git clone "${1}"
+pwd
+
+# 2. Clone the repo, if it does not already exist.
+if [ ! -d "${APP_NAME}" ]; then
+    git clone "${1}"
+fi
 
 # 3. Copy the Nginx config to the correct directory.
 NGINX_CONF_FILE="${APP_NAME}/webenv/nginx.conf"
@@ -19,7 +30,23 @@ else
     printf "Could not find a WebEnv Nginx config at the location ${NGINX_CONF_FILE} to copy.\n"
 fi
 
-# 4. Run any necessary configurations for the app from within the Docker app container.
+# 4. Run build steps if build script is present.
+BUILD_SCRIPT=$("${APP_NAME}/bin/build.sh")
+if [ -f "${BUILD_SCRIPT}" ]; then
+    docker exec "${APP_CONTAINER}" "cd" "/code/${APP_NAME}" "&&" "./bin/build.sh"
 
-# 5. Add the apps domain to hosts file
-# 6. Restart the NginX services
+else
+    printf "No build script found at the location ${BUILD_SCRIPT}.\n"
+fi
+
+
+# 5 Make an SSL certificate for the app.
+printf "Generating an named SSL certificate for the .\n"
+docker exec "${APP_CONTAINER}" "/generate-named-ssl-cert.sh" "${APP_NAME}.docker"
+
+# 6. Add the apps domain to host PCs' hosts file
+printf "Missing step to add '${APP_NAME}.docker' to the host PCs' host file.\n"
+
+# 7. Restart the NginX services
+printf "Restarting NginX service.\n"
+docker exec centos-nginx "nginx" "-s" "reload"
