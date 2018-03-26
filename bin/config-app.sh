f969b0804ff399d39b3cb6ed666bde9adb4f9fe0
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2018-03-26 09:58:22 -0400

    Updated config-app script with new features.

--- bin/config-app.sh
+++ bin/config-app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+# $1 = URL of a Git repository.
+# $2 = )(centos-apps|centos-apps54)
 # 1. Parse the app name, for ex: git@git:marketing-web/quickenloans.git => quickenloans
 APP_NAME=$(echo "${1}" | perl -pe 's#.+?/(.+?).git$#\1#g')
 # A sed version if perl is not available.
@@ -12,26 +14,56 @@ if [ ! -z "${2}" ]; then
     APP_CONTAINER="${2}"
 fi
 
+# Check APPS_DIR environment variable is defined.
+if [ -z "${APPS_DIR}" ]; then
+    printf "APPS_DIR is empty, setting to default ~/code\n"
+    APPS_DIR=$(echo "${HOME}/code")
+fi
+
+# Check NGINX_CONFS_DIR environment variable is defined.
+if [ -z "${NGINX_CONFS_DIR}" ]; then
+    printf "NGINX_CONFS_DIR is empty, setting to default ~/code/nginx-confs\n"
+    NGINX_CONFS_DIR=$(echo "${HOME}/code/nginx-confs")
+fi
+
 # 1.a Change to the docker apps dir.
 cd "${APPS_DIR}"
-pwd
+printf "CWD: " && pwd
 
 # 2. Clone the repo, if it does not already exist.
 if [ ! -d "${APP_NAME}" ]; then
     git clone "${1}"
 fi
 
-# 3. Copy the Nginx config to the correct directory.
-NGINX_CONF_FILE="${APP_NAME}/webenv/nginx.conf"
+# 3. Setup with Nginx container.
+NGINX_CONF_FILE=$(find "${HOME}/code/${APP_NAME}/web-env" -name "*-nginx.conf")
+echo "NGINX_CONF_FILE=${NGINX_CONF_FILE}"
+
 if [ -f "${NGINX_CONF_FILE}" ]; then
-    cp "${NGINX_CONF_FILE}" "${NGINX_CONFS_DIR}/${APP_NAME}.conf"
+    WEB_ENV_DIR=$(echo "${HOME}/code/${APP_NAME}/web-env/")
+    NGINX_NAME=$(echo "${NGINX_CONF_FILE}" | sed "s#${WEB_ENV_DIR}\(.*\)-nginx.conf\$#\1#")
+
+    # Copy the Nginx config to the correct directory.
+    printf "making copy: "
+    cp -v "${NGINX_CONF_FILE}" "${NGINX_CONFS_DIR}/${NGINX_NAME}.conf"
+
+    printf "Debug show all Nginx configs: "
     ls "${NGINX_CONFS_DIR}"
+
+    # Make an SSL certificate for the app.
+    printf "Generating an named SSL certificate for the app ${NGINX_NAME}.docker\n"
+    echo "APP_CONTAINER=${APP_CONTAINER}"
+
+    docker exec "${APP_CONTAINER}" "/generate-named-ssl-cert.sh" "${APP_NAME}.docker"
+
+    # Add the apps domain to host PCs' hosts file
+    printf "Missing step to add '${APP_NAME}.docker' to the host PCs' host file.\n"
 else
-    printf "Could not find a WebEnv Nginx config at the location ${NGINX_CONF_FILE} to copy.\n"
+    printf "Could not find an Nginx config at the location ${NGINX_CONF_FILE} to copy.\n"
 fi
 
 # 4. Run build steps if build script is present.
-BUILD_SCRIPT=$("${APP_NAME}/bin/build.sh")
+BUILD_SCRIPT="${APP_NAME}/bin/build.sh"
 if [ -f "${BUILD_SCRIPT}" ]; then
     docker exec "${APP_CONTAINER}" "cd" "/code/${APP_NAME}" "&&" "./bin/build.sh"
 
@@ -39,14 +71,6 @@ else
     printf "No build script found at the location ${BUILD_SCRIPT}.\n"
 fi
 
-
-# 5 Make an SSL certificate for the app.
-printf "Generating an named SSL certificate for the .\n"
-docker exec "${APP_CONTAINER}" "/generate-named-ssl-cert.sh" "${APP_NAME}.docker"
-
-# 6. Add the apps domain to host PCs' hosts file
-printf "Missing step to add '${APP_NAME}.docker' to the host PCs' host file.\n"
-
-# 7. Restart the NginX services
+# 6. Restart the NginX services
 printf "Restarting NginX service.\n"
 docker exec centos-nginx "nginx" "-s" "reload"
