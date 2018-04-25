76f9c8bad11e93ed38a13b101bbcacb5390b8109
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2018-04-25 04:02:11 -0400

    Updated Windows initial-setup script.

--- initial-setup.ps1
+++ initial-setup.ps1
@@ -1,5 +1,4 @@
-
-
+# Add environment variables.
 
 # Check APPS_DIR environment variable is defined.
 if (!$env:APPS_DIR) {
@@ -19,6 +18,8 @@ if (!$env:APPS_DIR) {
     printf "Environment variable APPS_DIR is empty, setting to ${APPS_DIR}`n"
 
     [Environment]::SetEnvironmentVariable("APPS_DIR", $APPS_DIR, "User")
+} else {
+    $APPS_DIR = $env:APPS_DIR
 }
 
 # Check NGINX_CONFS_DIR environment variable is defined.
@@ -28,3 +29,16 @@ if (!$env:NGINX_CONFS_DIR) {
     [Environment]::SetEnvironmentVariable("NGINX_CONFS_DIR", $NGINX_CONFS_DIR, "User")
 }
 
+# Check DOCKER_APPS_DIR environment variable is defined.
+if (!$env:DOCKER_APPS_DIR) {
+    $DOCKER_APPS_DIR="/code"
+    printf "Environment variable DOCKER_APPS_DIR is empty, setting to ${DOCKER_APPS_DIR}"
+    [Environment]::SetEnvironmentVariable("DOCKER_APPS_DIR", $DOCKER_APPS_DIR, "User")
+}
+
+# Check DOCKER_APPS_DIR environment variable is defined.
+if (!$env:DOCKER_APPS_DIR) {
+    $SSL_DIR="~/code/ssl"
+    printf "Environment variable SSL_DIR is empty, setting to ${SSL_DIR}"
+    [Environment]::SetEnvironmentVariable("SSL_DIR", $SSL_DIR, "User")
+}
