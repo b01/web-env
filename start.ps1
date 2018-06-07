bd89098b7f060aaefde403d5e20e5e278f993710
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2018-06-07 06:09:25 -0400

    Added parameters to start.ps1 command.

--- start.ps1
+++ start.ps1
@@ -3,7 +3,10 @@
 # - or -
 # Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
 #
-
+Param(
+    [string]$dcFile = '', # docker compose config file.
+    [string]$cname = '' #
+)
 pwd
 
 $DIR = split-path -parent $MyInvocation.MyCommand.Definition
@@ -14,9 +17,14 @@ $DIR = split-path -parent $MyInvocation.MyCommand.Definition
 . "${DIR}\build-env.ps1"
 
 $errorLog = "${DIR}/error.log"
+$conf = '';
+if ($dcFile) {
+    $conf = " -f ${dcFile}"
+}
 
 #docker-compose --project-name=web_env up -d --no-recreate --remove-orphans
-$DOCKER_COMPOSE_CMD="docker-compose --no-recreate --remove-orphans --project-name=web_env up"
+docker-compose --project-name=web_env up --no-recreate --remove-orphans
+#$DOCKER_COMPOSE_CMD="docker-compose${conf} --project-name=web_env up --no-recreate --remove-orphans"
 
 # Run docker compose in a new window.
-Start-Process -FilePath "powershell.exe" -Args $DOCKER_COMPOSE_CMD -Verb open -WorkingDirectory $DIR -RedirectStandardError $errorLog
+#Start-Process -FilePath "powershell.exe" -Args $DOCKER_COMPOSE_CMD -Verb open -WorkingDirectory $DIR -RedirectStandardError $errorLog
