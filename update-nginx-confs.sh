a8f3a0fe3fe12511ff895826e486d1b47a61cd7b
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2018-07-05 14:20:55 -0400

    Updated update-nginx-confs command to allow passing container as a parameter.

--- update-nginx-confs.sh
+++ update-nginx-confs.sh
@@ -1,6 +1,12 @@
 # Copy all applications nginx files into the mapped NginX vhost configuration directory.
 printf "Copying NginX configs over to mapped conatiner directory.\n"
-cp -v ~/code/*/web-env/*.conf ~/code/nginx-confs/
+
+cp -v "${APPS_DIR}"/*/web-env/*.conf "${APPS_DIR}"/nginx-confs/
+
+if [ -z "$1" ]; then
+ printf "Please specify an docker container as the first parameter.\n"
+ exit 1
+fi
 
 printf "Restarting NginX service.\n"
-docker exec centos-nginx nginx -s reload
+docker exec "${1}" nginx -s reload
