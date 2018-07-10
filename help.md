6f199b65119f604e164f1447ec5512a294367689
Khalifah Shabazz <khalifahshabazz@quickenloans.com>
2018-07-10 14:42:56 -0400

    Update webenv pipe command.

---
+++ help.md
@@ -0,0 +1,8 @@
+#Commands:
+
+ca      Configure application. Requires a git repo 
+        URL as it first argument.
+
+start   Start the web environment. With no options
+        this uses the default docker compose config
+        that comes with this tool.
--- web-env.sh
+++ web-env.sh
@@ -2,14 +2,27 @@
 
 WEB_ENV_DIR="${APPS_DIR}"/web-env
 
-command=$1
-commandParams=''
+command=''
 
-# Skip the first param since that is the actual command.
-shift
-#while [ "$1" != "" ]; do
-#  commandParams="${commandParams}${1} " && shift;
-#done;
+#filter and shorten the commands that can be run though this script.
+case $1 in
+    ca)
+        command="config-app"
+        break
+        ;;
+    start)
+        command=$1
+        break
+        ;;
+    *)
+        echo "Unknown command: ${1}. Please use one of the following:"
+        echo ""
+        cat "${WEB_ENV_DIR}"/help.md
+        ;;
+esac
 
-# Run the actual script and pass all arguments along.
-bash -e "${WEB_ENV_DIR}"/"${command}".sh $@
+# Run the actual script and pass all arguments (excluding the first) along.
+if [ -n "${command}" ]; then
+    shift
+    bash -e "${WEB_ENV_DIR}"/"${command}".sh $@
+fi
