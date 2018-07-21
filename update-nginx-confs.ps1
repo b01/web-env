d95ceee775007589751ea088d23faddbbb6b7e58
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2018-07-21 16:40:20 -0400

    Updated the PS update-ngixn-confs script to fail gracefully.

--- update-nginx-confs.ps1
+++ update-nginx-confs.ps1
@@ -4,18 +4,27 @@
 # Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
 
 Param(
-    [array]$container = "centos-nginx" # Allow a container to be specified.
+    [array]$container = "alpine-apps" # Allow a container to be specified.
 )
 
 $DIR = split-path -parent $MyInvocation.MyCommand.Definition
 
 . "$DIR\utilities.ps1"
 
-printf "CWD = ${DIR}`n"
+if (!$container) {
+    printf "Please specify a docker container as the first parameter.`n"
+    exit 1
+}
+
+$isUp=$(docker ps -aq -f "name=${container}" -f "status=running")
+if (!$isUp) {
+    printf "There is no container ${container} running. Please specify a running docker container as the first parameter.`n"
+    exit 1
+}
 
 # Copy all applications nginx files into the mapped NginX vhost configuration directory.
 printf "Copying NginX configs over to mapped container ${container} directory.`n"
-cp -v "${APPS_DIR}"\*\web-env\*.conf ~\code\nginx-confs\
+cp -v "${APPS_DIR}\*\web-env\*.conf" "${APPS_DIR}\nginx-confs\"
 
 printf "Restarting NginX service.`n"
 docker exec "${container}" nginx -s reload
