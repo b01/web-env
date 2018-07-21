23918ad84d11c04eb465e3854a44e5404fa5cb89
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2018-07-21 16:26:08 -0400

    Changed update-nginx-confs to check for container param first.

--- update-nginx-confs.sh
+++ update-nginx-confs.sh
@@ -1,12 +1,12 @@
 # Copy all applications nginx files into the mapped NginX vhost configuration directory.
 printf "Copying NginX configs over to mapped conatiner directory.\n"
 
-cp -v "${APPS_DIR}"/*/web-env/*.conf "${APPS_DIR}"/nginx-confs/
-
 if [ -z "$1" ]; then
  printf "Please specify an docker container as the first parameter.\n"
  exit 1
 fi
+
+cp -v "${APPS_DIR}"/*/web-env/*.conf "${APPS_DIR}"/nginx-confs/
 
 printf "Restarting NginX service.\n"
 docker exec "${1}" nginx -s reload
